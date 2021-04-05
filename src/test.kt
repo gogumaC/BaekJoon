54683121154eb07beb476471dc6fe6632916098e
yubinkim <kimu2371@naver.com>
2021-04-05 09:52:42 +0900

    #10809 알파벳 찾기
    문제
    알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
    
    출력
    각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
    
    만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
    
    https://www.acmicpc.net/problem/10809

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,26 @@
+import java.io.*
+import java.util.*
 fun main(){
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+    val S=br.readLine()
+    //val alphabet="abcdefghijklmnopqrstuvwxyz" -->일케 안하고 걍 아스키코드 쓰자구
+    var order= Array(26,{-1})
 
+
+    for(i in 0 until S.length){ //i in S 이상태에서는 인덱스 못 가져오나;;?
+
+        var alphbet=S[i]
+        var index=alphbet-'a'
+        if(order[index]==-1) {  //최초만 기록되야되니까 조건문 추가함; -1아니면 이미 한번 입력됬다는뜻이므로 여기서 걸러줌
+            order[index] = i
+        }
+    }
+
+
+    for(i in order){
+        bw.write("$i ")
+    }
+    br.close()
+    bw.close()
 }
