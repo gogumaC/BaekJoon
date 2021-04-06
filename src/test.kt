e871ac2cce7993dfe8c5c6f34c1a1457b8fccf93
yubinkim <kimu2371@naver.com>
2021-04-06 09:29:45 +0900

    #2675 문자열반복
    문제
    문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
    
    QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
    
    입력
    첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다. 
    
    출력
    각 테스트 케이스에 대해 P를 출력한다.
    
    https://www.acmicpc.net/problem/2675

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,26 @@
-fun main(){
+import java.io.*
+import java.lang.IndexOutOfBoundsException
 
+fun main(){
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+
+    val N=br.readLine().toInt()
+
+    for(n in 0 until N) {
+        val input=br.readLine().split(" ")
+        val R=input[0]
+        val S=input[1]
+
+        for (s in S) {
+            for (i in 0 until R.toInt()) { //while에서는 바로 i선언 못하나;
+                bw.write("$s")
+            }
+        }
+        bw.write("\n")
+        bw.flush()
+    }
+
+    br.close()
+    bw.close()
 }
