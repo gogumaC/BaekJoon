ded83e592b43b222fdfc28d2d8d1ad863b4f4254
yubinkim <kimu2371@naver.com>
2021-04-06 10:05:26 +0900

    #1157 단어공부
    문제
    알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
    
    입력
    첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
    
    출력
    첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
    
    https://www.acmicpc.net/problem/1157

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,53 @@
+import java.io.*
+
 fun main(){
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+
+    val word=br.readLine()
+    var alphbet=Array(26,{0})
+    val alphbets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for(char in word){
+        if(char>='A'&&char<'a'){    //소문자일때
+            alphbet[char-'A']++
+        }
+        else{   //대문자일때 ,왜인지는 모르겠지만 if else는 안써짐;;ㅜㅜ else if인가..?
+            alphbet[char-'a']++
+        }
+    }
+
+    try {
+        bw.write("${alphbets[getMax(alphbet)]}")
+    }catch(e:IndexOutOfBoundsException){
+        bw.write("?")
+    }
+
+    br.close()
+    bw.close()
 
 }
+
+fun getMax(alphbet:Array<Int>):Int{
+    var max=0
+    var maxcount=0
+    var result=0
+    for(i in 0 until alphbet.size){
+        if(alphbet[i]>max) {
+            max = alphbet[i]
+            result=i
+        }
+    }
+
+    for(i in alphbet){
+        if(i==max) maxcount++
+    }
+    if(maxcount>1) result=100
+    return result
+}
+
+/*
+ 필요기능
+  최댓값기능
+  문자 구분 ->오옹 아스키코드로 걍 해버리자 z-a Z-A 같으니까 이거로 같은 문자도 구분 가능할듯
+  문자 세기
+ */
