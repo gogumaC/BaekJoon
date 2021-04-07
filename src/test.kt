bc0f8f003f0397385831c2e2c1b422266b110290
yubinkim <kimu2371@naver.com>
2021-04-08 08:35:01 +0900

    #1152 단어의 개수
    문제
    영어 대소문자와 띄어쓰기만으로 이루어진 문자열이 주어진다. 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오. 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
    
    입력
    첫 줄에 영어 대소문자와 띄어쓰기로 이루어진 문자열이 주어진다. 이 문자열의 길이는 1,000,000을 넘지 않는다. 단어는 띄어쓰기 한 개로 구분되며, 공백이 연속해서 나오는 경우는 없다. 또한 문자열의 앞과 뒤에는 공백이 있을 수도 있다.
    
    출력
    첫째 줄에 단어의 개수를 출력한다.
    
    https://www.acmicpc.net/problem/1152

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,21 @@
-fun main(){
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
+fun main(){
+    val br= BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+    val wordList=br.readLine().split(" ")
+    var wordCount=0
+    for(word in wordList){
+        if(word !="")wordCount++
+    }
+    bw.write("$wordCount") //이거 ""안씌우고 돌렸더니 이상한문자가 나왔당;;
+    bw.flush()
+    //println(wordCount)
+    bw.close()
+    br.close()
 }
+
+//코드는 길어져도 buffer사용하는게 컴파일시간 단축됨
