3618a7446f22d36f71c8569f01d21a109f7b69a7
yubinkim <kimu2371@naver.com>
2021-04-08 08:57:59 +0900

    #2908 상수
    문제
    상근이의 동생 상수는 수학을 정말 못한다. 상수는 숫자를 읽는데 문제가 있다. 이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다. 상근이는 세 자리 수 두 개를 칠판에 써주었다. 그 다음에 크기가 큰 수를 말해보라고 했다.
    
    상수는 수를 다른 사람과 다르게 거꾸로 읽는다. 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
    
    두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 상근이가 칠판에 적은 두 수 A와 B가 주어진다. 두 수는 같지 않은 세 자리 수이며, 0이 포함되어 있지 않다.
    
    출력
    첫째 줄에 상수의 대답을 출력한다.
    
    https://www.acmicpc.net/problem/2908

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,42 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 fun main(){
 
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+
+    val nums=br.readLine().split(" ")
+    //nums[0],nums[1]이 각각의 숫자
+
+    val n1=changeNums(nums[0]).toInt()
+    val n2=changeNums(nums[1]).toInt()
+
+    bw.write("${compareNums(n1, n2)}")
+
+    br.close()
+    bw.close()
 }
+
+fun changeNums(num:String):String{
+    var reverseNum=""
+
+    for(i in 1 .. 3){
+       // reverseNum[i]=num[3-i] 엥 이게 왜 안돼누,,
+        reverseNum+=num[3-i]
+    }
+    return reverseNum
+}
+
+fun compareNums(n1:Int,n2:Int):Int{
+    if(n1>n2) return n1
+    else return n2
+}
+/*
+1. 두수 받기 (버퍼)
+2.
+3. 숫자 위치바꾸는 함수 구현
+4. 출력
+ */
