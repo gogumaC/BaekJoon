34780208029872f7d9f2aa62333842005992f2a4
yubinkim <kimu2371@naver.com>
2021-04-09 11:10:48 +0900

    #5622 다이얼
    문제
    상근이의 할머니는 아래 그림과 같이 오래된 다이얼 전화기를 사용한다.
    
    (그림)
    전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다. 숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
    
    숫자 1을 걸려면 총 2초가 필요하다. 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
    
    상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다. 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 예를 들어, UNUCIC는 868242와 같다.
    
    할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 알파벳 대문자로 이루어진 단어가 주어진다. 단어의 길이는 2보다 크거나 같고, 15보다 작거나 같다.
    
    출력
    첫째 줄에 다이얼을 걸기 위해서 필요한 최소 시간을 출력한다.
    
    https://www.acmicpc.net/problem/5622

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,49 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 fun main(){
 
+
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+
+    val word=br.readLine()
+    //val dial=arrayOf("","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ","")
+    var time=0
+    for(w in word){
+
+        when(w){
+
+            in 'A'..'C'->time+=3
+            in 'D'..'F'->time+=4
+            in 'G'..'I'->time+=5
+            in 'J'..'L'->time+=6
+            in 'M'..'O'->time+=7
+            in 'P'..'S'->time+=8
+            in 'T'..'V'->time+=9
+            in 'W'..'Z'->time+=10
+
+        }
+    }
+    bw.write("$time")
+    bw.flush()
+
+
+    bw.close()
+    br.close()
+
 }
+/*
+이 단어 최소시간
+1. 단어받기
+2.단어에 해당하는 숫자데이터만들기 (문자열로 배열만들자!)
+3.최소시간 구하기
+
+ */
+
+/*
+??배열이나 컬랙션에서 값알때 인덱스 어케찾을까??
+
+ */
