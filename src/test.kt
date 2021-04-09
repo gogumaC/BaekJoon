57af8a0bf4b8eef4faf7d77caacaa4ae9e8ceabb
yubinkim <kimu2371@naver.com>
2021-04-09 12:00:56 +0900

    #2941 크로아티아 알파벳
    문제
    예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다.
    
    크로아티아 알파벳	변경
    č	c=
    ć	c-
    dž	dz=
    đ	d-
    lj	lj
    nj	nj
    š	s=
    ž	z=
    예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
    
    dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
    
    입력
    첫째 줄에 최대 100글자의 단어가 주어진다. 알파벳 소문자와 '-', '='로만 이루어져 있다.
    
    단어는 크로아티아 알파벳으로 이루어져 있다. 문제 설명의 표에 나와있는 알파벳은 변경된 형태로 입력된다.
    
    https://www.acmicpc.net/problem/2941

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,38 @@
-fun main(){
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
+fun main(){
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+    val word=br.readLine()
+    val croatia=arrayOf("c=","c-","dz=","d-","lj","nj","s=","z=")
+    var newWord=word
+
+    for(cro in croatia){
+        if(word.contains(cro)){
+            newWord=newWord.replace(cro,"c")
+        }
+    }
+
+
+    bw.write("${newWord.length}")
+    bw.flush()
+    br.close()
+    bw.close()
 }
+
+/*
+1. 단어받음
+2. 단어안에 각각의 크로아티아 알파벳 포함되있는지 확인
+    [1안]
+    첫번쨰 문자확인->두번째문자확인->세번쨰문자확인
+    [2안]
+    split으로 크로아티아 알파벳기준 문자열나누기
+    [3안]
+    !!!!.replace사용해서 대체하기!!!!
+
+3. 있으면 그부분을 다른 하나의 문자로 변환
+4. length로 알파벳세서 출력
+ */
