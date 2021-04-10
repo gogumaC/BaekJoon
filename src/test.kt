d0c6c36430b7da525808de051dfbf5d7e899f0ac
yubinkim <kimu2371@naver.com>
2021-04-10 11:38:44 +0900

    #1712 손익분기점
    문제
    월드전자는 노트북을 제조하고 판매하는 회사이다. 노트북 판매 대수에 상관없이 매년 임대료, 재산세, 보험료, 급여 등 A만원의 고정 비용이 들며, 한 대의 노트북을 생산하는 데에는 재료비와 인건비 등 총 B만원의 가변 비용이 든다고 한다.
    
    예를 들어 A=1,000, B=70이라고 하자. 이 경우 노트북을 한 대 생산하는 데는 총 1,070만원이 들며, 열 대 생산하는 데는 총 1,700만원이 든다.
    
    노트북 가격이 C만원으로 책정되었다고 한다. 일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다. 최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
    
    A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 21억 이하의 자연수이다.
    
    출력
    첫 번째 줄에 손익분기점 즉 최초로 이익이 발생하는 판매량을 출력한다. 손익분기점이 존재하지 않으면 -1을 출력한다.
    
    https://www.acmicpc.net/problem/1712

--- src/test.kt
+++ src/test.kt
@@ -6,50 +6,33 @@ import java.io.OutputStreamWriter
 fun main(){
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+    val inf=br.readLine().split(" ")
 
-    val N=br.readLine().toInt()
-    val words=Array(N,{br.readLine()})  //호옹이!!! 혁명이다!!!
-    var count=0
-
-    for(i in 0 until N){
-        if(checkGroup(words[i]))count++
-    }
-    bw.write("$count")
-
+    var n=findBreakEvenPoint(inf[0].toLong(), inf[1].toLong(), inf[2].toLong())
+    bw.write("$n")
     bw.flush()
-    br.close()
     bw.close()
+    br.close()
+
+
 }
 
-fun checkGroup(word:String):Boolean{
+fun findBreakEvenPoint(fixCost:Long,flexibleCost:Long,price:Long):Long{
 
-    var letters = ""
-    for(j in word.indices) {
-        //aabbcca
-        try {
-            if (word[j - 1] != word[j]) {
-                if (!letters.contains(word[j])) letters += word[j]
-                else {
-                    return false
-                }
-            }
-
-
-        } catch (e: StringIndexOutOfBoundsException) {
-            letters += word[j]
-        }
+    if(price>flexibleCost) {
+        var n=fixCost/(price-flexibleCost)
+        return n+1
     }
-    return true
-}
+    else return -1
 
+
+}
 /*
-1.단어개수받기
-2. 단어받기
-3. 그룹단어 구분- 같은문자가 떨어져있으면 그룹단어아님
-    -어떤 글자 있었는지 기억해야됨
-    [1안] 1. 나온글자 안겹치게 다른 변수에 순서대로저장(앞뒤 글자 같은경우는 미리 조건문으로 제거)
-            2.앞에 글자같은지 확인(뒤에 또시작하는 애들은 어차피 앞에가 항상 다를테니까)
-            3. 안똑같으면 letter에 글자있는지 확인 contain
-            4. 없으면 걍 넘어가고 있으면 카운트 증가
-4.출력
+고정비용A ,가변비용B , 판매가격C, 판매대수 N
+손익분기점 : 총수입(C*N)=고정비용A+B*N
+없으면 -1출력
+
+[다른답안 참고] : 하나씩 올리면 시간이 어마무시하게 걸리니까 수식을 써야된다!
+ 손익분기점 -> 실제얻는 소득(가변비용-고정비용)*N=고정비용
+  따라서 (C-B)*N=A  -> N=A/(C-B)!
  */
