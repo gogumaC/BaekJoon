4021f95219f260f7d55d061f035c2a9b0e912fb6
yubinkim <kimu2371@naver.com>
2021-04-12 11:23:08 +0900

    #1193 분수 찾기
    문제
    무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
    
    1/1	1/2	1/3	1/4	1/5	…
    2/1	2/2	2/3	2/4	…	…
    3/1	3/2	3/3	…	…	…
    4/1	4/2	…	…	…	…
    5/1	…	…	…	…	…
    …	…	…	…	…	…
    이와 같이 나열된 분수들을 1/1 -> 1/2 -> 2/1 -> 3/1 -> 2/2 -> … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
    
    X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
    
    출력
    첫째 줄에 분수를 출력한다.
    
    https://www.acmicpc.net/problem/1193

--- src/test.kt
+++ src/test.kt
@@ -4,33 +4,49 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 
-//#2292
+//#1193
 fun main(){
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+
     val num=br.readLine().toInt()
-    var stage=1 //반복문안에서 선언하면 메모리에 안좋을까..?
+    var groupIndex=1
+    var groupCount:Int
+    while(true){
+        groupCount=groupIndex*(groupIndex+1)/2
 
-
-    while(true){   //걍 while안으로 안들어오는데;; -> 조건문제였음ㅜㅜㅜㅜ
-        var n=1+3*stage*(stage-1)
-        if(n>=num) break
-        else stage++
+        if(num<=groupCount){
+            bw.write(getFraction(num,groupIndex,groupCount))
+            break
+        }
+        else groupIndex++
     }
-
-    //println(" n : $n , st: $stage")
-    bw.write("$stage")
-
 
     br.close()
     bw.close()
 
 }
+fun getFraction(n:Int,index:Int,count:Int):String{
+    var order=n-(count-index) //count-index=precount
+    //println(order)
+    var numerator=if(index%2==0)order
+                    else index-order+1
+    var denominator=if(index%2==0)index-order+1
+                    else order
+
+    return "$numerator/$denominator"
+}
 
 /*
-6각형
-1+6+12+18...
---> 1+6*시그마k (k=1~stage까지)
--->등차수열공식 시그마k(k=1~n까지)=1/2*n*(n+1)
+ 분자 1/-1-2/-1-2-3/-1-2-3-4/-...
+ 분모 1/-2-1/-3-2-1/-4-3-2-1/-...
+
+ 등차수열 공식이용하면 (g=그룹 인덱스,그룹별 숫자 수)
+ g그룹까지의 전체 개수= 1/2*g*(g+1)
+
+ -->자기순서=n-그전 그룹까지의 개수
+ 분자 : (index짝수면)자기순서 (홀수면)역수
+ ---------------------------------------
+ 분모 : (짝수면)그 그룹 인덱스(개수)-자기순서(홀수면)자기순서
  */
 
