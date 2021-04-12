2b9351d5b247a90bd0fec06b420b7c0118b273ae
yubinkim <kimu2371@naver.com>
2021-04-12 10:32:05 +0900

    #2992 벌집
    문제
    
    (그림)
    위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다. 숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
    
    입력
    첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
    
    출력
    입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
    
    https://www.acmicpc.net/problem/2292

--- src/test.kt
+++ src/test.kt
@@ -3,36 +3,34 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
+
+//#2292
 fun main(){
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
-    val inf=br.readLine().split(" ")
-
-    var n=findBreakEvenPoint(inf[0].toLong(), inf[1].toLong(), inf[2].toLong())
-    bw.write("$n")
-    bw.flush()
-    bw.close()
-    br.close()
+    val num=br.readLine().toInt()
+    var stage=1 //반복문안에서 선언하면 메모리에 안좋을까..?
 
 
-}
-
-fun findBreakEvenPoint(fixCost:Long,flexibleCost:Long,price:Long):Long{
-
-    if(price>flexibleCost) {
-        var n=fixCost/(price-flexibleCost)
-        return n+1
+    while(true){   //걍 while안으로 안들어오는데;; -> 조건문제였음ㅜㅜㅜㅜ
+        var n=1+3*stage*(stage-1)
+        if(n>=num) break
+        else stage++
     }
-    else return -1
 
+    //println(" n : $n , st: $stage")
+    bw.write("$stage")
+
+
+    br.close()
+    bw.close()
 
 }
-/*
-고정비용A ,가변비용B , 판매가격C, 판매대수 N
-손익분기점 : 총수입(C*N)=고정비용A+B*N
-없으면 -1출력
 
-[다른답안 참고] : 하나씩 올리면 시간이 어마무시하게 걸리니까 수식을 써야된다!
- 손익분기점 -> 실제얻는 소득(가변비용-고정비용)*N=고정비용
-  따라서 (C-B)*N=A  -> N=A/(C-B)!
+/*
+6각형
+1+6+12+18...
+--> 1+6*시그마k (k=1~stage까지)
+-->등차수열공식 시그마k(k=1~n까지)=1/2*n*(n+1)
  */
+
