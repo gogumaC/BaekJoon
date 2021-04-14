f350d640a73697d9a1ef5df1caa000b84aa9bc17
yubinkim <kimu2371@naver.com>
2021-04-14 11:18:52 +0900

    #2775 부녀회장이 될테야
    문제
    평소 반상회에 참석하는 것을 좋아하는 주희는 이번 기회에 부녀회장이 되고 싶어 각 층의 사람들을 불러 모아 반상회를 주최하려고 한다.
    
    이 아파트에 거주를 하려면 조건이 있는데, “a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다” 는 계약 조항을 꼭 지키고 들어와야 한다.
    
    아파트에 비어있는 집은 없고 모든 거주민들이 이 계약 조건을 지키고 왔다고 가정했을 때, 주어지는 양의 정수 k와 n에 대해 k층에 n호에는 몇 명이 살고 있는지 출력하라. 단, 아파트에는 0층부터 있고 각층에는 1호부터 있으며, 0층의 i호에는 i명이 산다.
    
    입력
    첫 번째 줄에 Test case의 수 T가 주어진다. 그리고 각각의 케이스마다 입력으로 첫 번째 줄에 정수 k, 두 번째 줄에 정수 n이 주어진다
    
    출력
    각각의 Test case에 대해서 해당 집에 거주민 수를 출력하라.
    
    제한
    1 ≤ k, n ≤ 14
    
    https://www.acmicpc.net/problem/2775

--- src/test.kt
+++ src/test.kt
@@ -4,37 +4,17 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 
-//#10250
+//#2775
 fun main() {
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-
-    val testCase = br.readLine().toInt()
-    for (i in 0 until testCase) {
-
-        var inf = br.readLine().split(" ")
-        var roomNum :String
-        // inf[0] : 층수 , inf[1] : 방 수 , inf[2] : 몇번째손님
-        var floor:Int
-        var room:Int
-
-        when(inf[2].toInt() % inf[0].toInt()){
-            0->{
-                floor=inf[0].toInt()
-                room=inf[2].toInt() / inf[0].toInt()
-            }
-            else->{
-                floor=inf[2].toInt() % inf[0].toInt()
-                room=inf[2].toInt() / inf[0].toInt() + 1
-            }
-        }
-
-        roomNum = "$floor${
-            if (room < 10) "0$room"
-            else "$room"
-        }"
-        bw.write("$roomNum\n")
+    for(i in 0 until br.readLine().toInt()){
+        val floor=br.readLine().toInt()
+        val room=br.readLine().toInt()
+        var people=Array(floor+1, {Array(room,{0})})
+        // bw.write("${calPeople(floor,room)}\n")
+        bw.write("${calArray(people)[floor][room-1]}\n")
         bw.flush()
 
     }
@@ -42,18 +22,43 @@ fun main() {
     br.close()
     bw.close()
 
+
+}
+
+fun calPeople(floor:Int,room:Int):Int{  //재귀함수 이용 ->오래걸림
+    var people=0
+    if(floor==0){
+        people=room
+    }
+    else {
+        for (r in 1..room) {
+            people+= calPeople(floor - 1, r)
+        }
+    }
+    return people
+}
+
+fun calArray(people:Array<Array<Int>>):Array<Array<Int>>{ //2차원 배열사용
+    for(i in people[0].indices){
+        people[0][i]=i+1
+    }
+
+    for(i in people.indices) people[i][0]=1
+
+    for(i in 1 until people.size){
+        //p(f,r)=p(f,r-1)+p(f-1,r)
+        for(j in 1 until people[i].size){
+            people[i][j]=people[i][j-1]+people[i-1][j]
+        }
+    }
+
+    return people
 }
 
 /*
-T : 테스트 케이스 수,
-H(층 수), W(방 수), N(몇번째 손님)
+테스트케이스
+k층 n호->아래층 1~b호까지 사람들의 수의 합만큼 사람을 데려와 살아야함
 
-  가장짧은거리방
-  엘리베이터 가장 왼쪽
-  HxW 형태 호텔
-  두 방사이의 거리=1
-  방번호 :층수,엘베부터 세었을때 번호
-  걷는 거리가 같을때는 아래층방선호
-  첫번째 101 ->201
+0층 i명
  */
 
