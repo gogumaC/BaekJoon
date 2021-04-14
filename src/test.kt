b667ed155cdafb22e450e333f55302dc99db962a
yubinkim <kimu2371@naver.com>
2021-04-14 11:47:44 +0900

    #2839 설탕배달
    문제
    상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다. 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
    
    상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만, 5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
    
    상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 N이 주어진다. (3 ≤ N ≤ 5000)
    
    출력
    상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.
    
    https://www.acmicpc.net/problem/2839

--- src/test.kt
+++ src/test.kt
@@ -9,15 +9,9 @@ fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
-    for(i in 0 until br.readLine().toInt()){
-        val floor=br.readLine().toInt()
-        val room=br.readLine().toInt()
-        var people=Array(floor+1, {Array(room,{0})})
-        // bw.write("${calPeople(floor,room)}\n")
-        bw.write("${calArray(people)[floor][room-1]}\n")
-        bw.flush()
+    val flourMass=br.readLine().toInt()
 
-    }
+    bw.write("${my(flourMass)}")
 
     br.close()
     bw.close()
@@ -25,40 +19,36 @@ fun main() {
 
 }
 
-fun calPeople(floor:Int,room:Int):Int{  //재귀함수 이용 ->오래걸림
-    var people=0
-    if(floor==0){
-        people=room
-    }
-    else {
-        for (r in 1..room) {
-            people+= calPeople(floor - 1, r)
+fun my(flourMass:Int):Int{ //132ms
+    var count=-1
+    for(x in 0..flourMass/3) {
+        if((flourMass-3*x)%5==0) {
+            count = ((flourMass - 3 * x) / 5)+x
+            break
         }
     }
-    return people
+    return count
 }
 
-fun calArray(people:Array<Array<Int>>):Array<Array<Int>>{ //2차원 배열사용
-    for(i in people[0].indices){
-        people[0][i]=i+1
-    }
-
-    for(i in people.indices) people[i][0]=1
-
-    for(i in 1 until people.size){
-        //p(f,r)=p(f,r-1)+p(f-1,r)
-        for(j in 1 until people[i].size){
-            people[i][j]=people[i][j-1]+people[i-1][j]
+fun otherPerson(flourMass:Int):Int{ //68ms
+    var n =flourMass
+    var count=0
+    while(true){
+        if(n%5==0){
+            return n/5+count
+            break
         }
+        else if(n<=0){
+            return -1
+            break
+        }
+        n-=3
+        count++
     }
-
-    return people
 }
+
 
 /*
-테스트케이스
-k층 n호->아래층 1~b호까지 사람들의 수의 합만큼 사람을 데려와 살아야함
 
-0층 i명
  */
 
