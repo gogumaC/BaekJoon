a231b52c850bbd2dbfded9b58c42dc83be071517
yubinkim <kimu2371@naver.com>
2021-04-16 13:24:22 +0900

    #1011 Fly me to the Alpha Centauri
    문제
    우현이는 어린 시절, 지구 외의 다른 행성에서도 인류들이 살아갈 수 있는 미래가 오리라 믿었다. 그리고 그가 지구라는 세상에 발을 내려 놓은 지 23년이 지난 지금, 세계 최연소 ASNA 우주 비행사가 되어 새로운 세계에 발을 내려 놓는 영광의 순간을 기다리고 있다.
    
    그가 탑승하게 될 우주선은 Alpha Centauri라는 새로운 인류의 보금자리를 개척하기 위한 대규모 생활 유지 시스템을 탑재하고 있기 때문에, 그 크기와 질량이 엄청난 이유로 최신기술력을 총 동원하여 개발한 공간이동 장치를 탑재하였다. 하지만 이 공간이동 장치는 이동 거리를 급격하게 늘릴 경우 기계에 심각한 결함이 발생하는 단점이 있어서, 이전 작동시기에 k광년을 이동하였을 때는 k-1 , k 혹은 k+1 광년만을 다시 이동할 수 있다. 예를 들어, 이 장치를 처음 작동시킬 경우 -1 , 0 , 1 광년을 이론상 이동할 수 있으나 사실상 음수 혹은 0 거리만큼의 이동은 의미가 없으므로 1 광년을 이동할 수 있으며, 그 다음에는 0 , 1 , 2 광년을 이동할 수 있는 것이다. ( 여기서 다시 2광년을 이동한다면 다음 시기엔 1, 2, 3 광년을 이동할 수 있다. )
    
    
    
    김우현은 공간이동 장치 작동시의 에너지 소모가 크다는 점을 잘 알고 있기 때문에 x지점에서 y지점을 향해 최소한의 작동 횟수로 이동하려 한다. 하지만 y지점에 도착해서도 공간 이동장치의 안전성을 위하여 y지점에 도착하기 바로 직전의 이동거리는 반드시 1광년으로 하려 한다.
    
    김우현을 위해 x지점부터 정확히 y지점으로 이동하는데 필요한 공간 이동 장치 작동 횟수의 최솟값을 구하는 프로그램을 작성하라.
    
    입력
    입력의 첫 줄에는 테스트케이스의 개수 T가 주어진다. 각각의 테스트 케이스에 대해 현재 위치 x 와 목표 위치 y 가 정수로 주어지며, x는 항상 y보다 작은 값을 갖는다. (0 ≤ x < y < 231)
    
    출력
    각 테스트 케이스에 대해 x지점으로부터 y지점까지 정확히 도달하는데 필요한 최소한의 공간이동 장치 작동 횟수를 출력한다.
    
    https://www.acmicpc.net/problem/1011

--- src/test.kt
+++ src/test.kt
@@ -5,42 +5,80 @@ import java.io.OutputStreamWriter
 import java.math.BigInteger
 
 
-//#10757
+//#1011
 fun main() {
-    val br=BufferedReader(InputStreamReader(System.`in`))
-    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
 
-    val nums=br.readLine().split(" ")
-    val sum=bigInteger(nums)
+    for (i in 0 until br.readLine().toInt()) {
+        val (x, y) = br.readLine().split(" ")
+        val count=getCount(x.toLong(),y.toLong())
+        bw.write("${count}\n")
+        bw.flush()
 
 
-    bw.write("${sum}")
+    }
 
 
     br.close()
     bw.close()
 }
 
-fun bigInteger(nums:List<String>): BigInteger {
-    val sum=nums[0].toBigInteger()+nums[1].toBigInteger()
-    return sum
+fun getCount(x:Long,y:Long):Long{
+    val gap=y-x
+    val halfGap=gap/2
+    var halfCount=1L
+    var tempUnder:Long
+    var count=0L
+    val total:Long
+    if (gap==1L) return 1
+    else {
+        while (true) {
+            tempUnder = halfCount * (halfCount + 1) / 2L
+            if (tempUnder == halfGap) {
+                total=2*tempUnder
+                break
+            } else if (tempUnder > halfGap) {
+                total=2*(tempUnder-halfCount)
+                halfCount--
+                break
+            }
+            halfCount++
+        }
+
+
+        val rest=gap-total
+
+
+        count+=when {
+            rest==0L->0
+            rest<=halfCount+1L->1
+            rest>halfCount+1->2
+            else->0
+
+        }
+
+        count += halfCount * 2
+
+
+
+        return count
+    }
 }
 
-//fun double(nums: List<String>):String{ //끝까지 출력 못한다ㅜㅜ
-//    val sumString=(nums[0].toDouble()+nums[1].toDouble()).toString()
-//    println(sumString)
-//    var newSum=sumString.split("E")
-//    var sum=newSum[0].replace(".","")
-//
-//    println("float : ${sum}")
-//    return ""
-//}
-
-
-//다른사람은 배열로 출력해냄! 대단..!
 
 /*
-    a,b입력 ->a+b출력
+   테스트 케이스T / 현재위치 x , 목표위치 y (x<y)
+   x=0 이동가능 +1
+   x=1 이동가능 +1,2
+   x=3 이동가능 +2,3,4
+   ---
+   마지막이동은 무조건 1
+
+   처음 , 마지막 둘다 1로 끝나야하므로 낙타 봉우리모양 그래프로 속력을 표기가능/ 따라서 절반만 구해서 *2 하겠음 -> 예외있음(거리가 홀수일때)
+   등비수열 k+1로 gap의 가장 가까운 바로 밑수까지 구하고 부족하면 중간에 더할수있으므로 count++
+   -> 예를들어 절반 거리가 11일때 가장가까운 밑수는 1+2+3+4=10 이고, 문제 설명상 중간에 k->k로 이동해도 관계없으므로
+     1+'1'+2+3+4=11로 중간에 1을 추가해서 거리 맞추고 count++
  */
 
