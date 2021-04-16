b1d83084623d066119f993c57797ff0fe6448ca6
yubinkim <kimu2371@naver.com>
2021-04-16 13:47:55 +0900

    #1978 소수 찾기
    문제
    주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
    
    입력
    첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
    
    출력
    주어진 수들 중 소수의 개수를 출력한다.
    
    https://www.acmicpc.net/problem/1978

--- src/test.kt
+++ src/test.kt
@@ -5,80 +5,51 @@ import java.io.OutputStreamWriter
 import java.math.BigInteger
 
 
-//#1011
+//#1978
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+    val br=BufferedReader(InputStreamReader(System.`in`))
+    val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
+    val N=br.readLine().toInt()
+    var nums=br.readLine().split(" ")
 
-    for (i in 0 until br.readLine().toInt()) {
-        val (x, y) = br.readLine().split(" ")
-        val count=getCount(x.toLong(),y.toLong())
-        bw.write("${count}\n")
-        bw.flush()
+    var count=0
 
+    for(n in nums){
 
+        if (n.toInt()!=1&&checkNum(n.toInt())) count++
     }
+
+    bw.write("$count")
+
 
 
     br.close()
     bw.close()
+
+
+
 }
 
-fun getCount(x:Long,y:Long):Long{
-    val gap=y-x
-    val halfGap=gap/2
-    var halfCount=1L
-    var tempUnder:Long
-    var count=0L
-    val total:Long
-    if (gap==1L) return 1
-    else {
-        while (true) {
-            tempUnder = halfCount * (halfCount + 1) / 2L
-            if (tempUnder == halfGap) {
-                total=2*tempUnder
-                break
-            } else if (tempUnder > halfGap) {
-                total=2*(tempUnder-halfCount)
-                halfCount--
-                break
-            }
-            halfCount++
-        }
+fun checkNum(n:Int):Boolean{
 
-
-        val rest=gap-total
-
-
-        count+=when {
-            rest==0L->0
-            rest<=halfCount+1L->1
-            rest>halfCount+1->2
-            else->0
-
-        }
-
-        count += halfCount * 2
-
-
-
-        return count
+    for(div in 2 until n){
+        if(n%div==0) return false
     }
+    return true
 }
+
 
 
 /*
-   테스트 케이스T / 현재위치 x , 목표위치 y (x<y)
-   x=0 이동가능 +1
-   x=1 이동가능 +1,2
-   x=3 이동가능 +2,3,4
-   ---
-   마지막이동은 무조건 1
+소수출력 프로그램
+숫자개수 N<100
+숫자<1000 자연수
+->소수개수 출력
 
-   처음 , 마지막 둘다 1로 끝나야하므로 낙타 봉우리모양 그래프로 속력을 표기가능/ 따라서 절반만 구해서 *2 하겠음 -> 예외있음(거리가 홀수일때)
-   등비수열 k+1로 gap의 가장 가까운 바로 밑수까지 구하고 부족하면 중간에 더할수있으므로 count++
-   -> 예를들어 절반 거리가 11일때 가장가까운 밑수는 1+2+3+4=10 이고, 문제 설명상 중간에 k->k로 이동해도 관계없으므로
-     1+'1'+2+3+4=11로 중간에 1을 추가해서 거리 맞추고 count++
+소수: 소인수가 자기랑 1밖에 없는 친구 (! 1은 소수아님 !)
+
+
+엥 로직은 비슷한데 다른사람들보다 꽤 오래 걸린다-> 아마 자료 입출력에서 시간이 걸리나보당,,
  */
 
