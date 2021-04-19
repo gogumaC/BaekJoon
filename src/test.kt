e4bffb0a494b09b52f80d4dea1189930786a8cee
yubinkim <kimu2371@naver.com>
2021-04-19 11:45:45 +0900

    #9020 골드바흐의 추측
    문제
    1보다 큰 자연수 중에서  1과 자기 자신을 제외한 약수가 없는 자연수를 소수라고 한다. 예를 들어, 5는 1과 5를 제외한 약수가 없기 때문에 소수이다. 하지만, 6은 6 = 2 × 3 이기 때문에 소수가 아니다.
    
    골드바흐의 추측은 유명한 정수론의 미해결 문제로, 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다는 것이다. 이러한 수를 골드바흐 수라고 한다. 또, 짝수를 두 소수의 합으로 나타내는 표현을 그 수의 골드바흐 파티션이라고 한다. 예를 들면, 4 = 2 + 2, 6 = 3 + 3, 8 = 3 + 5, 10 = 5 + 5, 12 = 5 + 7, 14 = 3 + 11, 14 = 7 + 7이다. 10000보다 작거나 같은 모든 짝수 n에 대한 골드바흐 파티션은 존재한다.
    
    2보다 큰 짝수 n이 주어졌을 때, n의 골드바흐 파티션을 출력하는 프로그램을 작성하시오. 만약 가능한 n의 골드바흐 파티션이 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
    
    입력
    첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고 짝수 n이 주어진다.
    
    출력
    각 테스트 케이스에 대해서 주어진 n의 골드바흐 파티션을 출력한다. 출력하는 소수는 작은 것부터 먼저 출력하며, 공백으로 구분한다.
    
    제한
    4 ≤ n ≤ 10,000
    
    https://www.acmicpc.net/problem/9020

--- src/test.kt
+++ src/test.kt
@@ -5,37 +5,73 @@ import java.io.OutputStreamWriter
 import kotlin.math.sqrt
 
 
-//#4948
+//#9020
 fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
 
-    while(true){
+    val primes=get10000Primes()
+
+    for(case in 0 until br. readLine().toInt()){
+
         var n=br.readLine().toInt()
-        if(n==0)break
-        var count=getPrimes(n)
-        bw.write("$count\n")
+        var half=n/2
+        while(true){
+
+            if(primes[half]&&primes[n-half]) {
+                bw.write("$half ${n-half}\n")
+                break
+            }
+            half--
+        }
+
         bw.flush()
 
+
+
     }
+
+
+
     br.close()
     bw.close()
 
 }
 
+fun get10000Primes():Array<Boolean>{
+    val n=10000
+    var array=Array<Boolean>(n+1,{true})
+    array[0]=false
+    array[1]=false
 
-
-fun getPrimes(n:Int):Int{
-    var count=0
-    go@ for(num in n+1..2*n){
-        for(i in 2..sqrt(num.toDouble()).toInt()){
-            if(num%i==0) continue@go
+    for(num in 2..sqrt(n.toDouble()).toInt()){
+        var i=2
+        if(array[num]){
+            while(num*i<=n){
+                array[num*i]=false
+                i++
+            }
         }
-        count++
+
     }
-    return count
+    return array
 }
 
-//에라토리스 체 너무 오래걸림
-//다른 사람들은 범위 사용해서 미리 입력값 범위까지 프라임 먼저 구해놓고 출력만 하는식으로 구현
+
+/*
+골드바흐 추측 : 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼수있다.
+
+n:2보다 큰 짝수
+결과 많을때는 차이 적게나는걸로
+
+소수
+
+1안 : 앞 소수부터 순서대로 더해서 찾기
+2안 : 가장가까운 소수부터 빼기
+*3안 : 가운데 부터 양쪽 소수 더해서 찾기
+
+걍 boolean배열이나 숫자만 있나 별 차이없었음
+다른사람은 나처럼 두개 수를 따로 조작한게 아니라 half랑 n-half랑 둘다 참이면 걍 통과 아니면 half-- 해서 숫자하나만 써서 구현
+이게 나처럼 증가하는 로직 하나 감소하는 로직하나 짜는것보다 훨간단하고 가독성도 좋은듯
+ */
