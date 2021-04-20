06bd147740923777bcf5c62357953c5aa0f88431
yubinkim <kimu2371@naver.com>
2021-04-20 09:44:34 +0900

    #1085 직사각형에서 탈출
    문제
    한수는 지금 (x, y)에 있다. 직사각형의 왼쪽 아래 꼭짓점은 (0, 0)에 있고, 오른쪽 위 꼭짓점은 (w, h)에 있다. 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 x, y, w, h가 주어진다.
    
    출력
    첫째 줄에 문제의 정답을 출력한다.
    
    제한
    1 ≤ w, h ≤ 1,000
    1 ≤ x ≤ w-1
    1 ≤ y ≤ h-1
    x, y, w, h는 정수
    
    https://www.acmicpc.net/problem/1085

--- src/test.kt
+++ src/test.kt
@@ -10,68 +10,30 @@ fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
+    val (x, y, w, h) = br.readLine().split(" ")
 
-    val primes=get10000Primes()
-
-    for(case in 0 until br. readLine().toInt()){
-
-        var n=br.readLine().toInt()
-        var half=n/2
-        while(true){
-
-            if(primes[half]&&primes[n-half]) {
-                bw.write("$half ${n-half}\n")
-                break
-            }
-            half--
-        }
-
-        bw.flush()
-
-
-
-    }
-
-
-
+    bw.write("${findMin(arrayOf(x.toInt(),y.toInt(),w.toInt()-x.toInt(),h.toInt()-y.toInt()))}")
     br.close()
     bw.close()
 
 }
 
-fun get10000Primes():Array<Boolean>{
-    val n=10000
-    var array=Array<Boolean>(n+1,{true})
-    array[0]=false
-    array[1]=false
-
-    for(num in 2..sqrt(n.toDouble()).toInt()){
-        var i=2
-        if(array[num]){
-            while(num*i<=n){
-                array[num*i]=false
-                i++
-            }
+fun findMin(nums:Array<Int>):Int{
+    var min=1000
+    for(num in nums){
+        if(num<min){
+            min=num
         }
-
     }
-    return array
+    return min
 }
 
 
+
+
 /*
-골드바흐 추측 : 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼수있다.
+입력 : x y w h
+출력 : 직사각형 경계면까지의 최소거리
 
-n:2보다 큰 짝수
-결과 많을때는 차이 적게나는걸로
-
-소수
-
-1안 : 앞 소수부터 순서대로 더해서 찾기
-2안 : 가장가까운 소수부터 빼기
-*3안 : 가운데 부터 양쪽 소수 더해서 찾기
-
-걍 boolean배열이나 숫자만 있나 별 차이없었음
-다른사람은 나처럼 두개 수를 따로 조작한게 아니라 half랑 n-half랑 둘다 참이면 걍 통과 아니면 half-- 해서 숫자하나만 써서 구현
-이게 나처럼 증가하는 로직 하나 감소하는 로직하나 짜는것보다 훨간단하고 가독성도 좋은듯
+위치가 (x,y)일때 직사각형까지 가는 4개의 직선거리는 x,y,w-x,h-y이므로 이 네개의 숫자중 최솟값을 구해서 출력
  */
