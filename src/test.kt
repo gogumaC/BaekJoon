d46c68d47f2d5b49867e4e3484684dcb2461dedb
yubinkim <kimu2371@naver.com>
2021-04-20 10:12:02 +0900

    #3009 보충

--- src/test.kt
+++ src/test.kt
@@ -10,11 +10,11 @@ fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
-    val n1=br.readLine().split(" ")
-    val n2=br.readLine().split(" ")
-    val n3=br.readLine().split(" ")
-    val x=findDif(n1[0].toInt(),n2[0].toInt(),n3[0].toInt())
-    val y=findDif(n1[1].toInt(),n2[1].toInt(),n3[1].toInt())
+    val n1=br.readLine().split(" ").map{it.toInt()}
+    val n2=br.readLine().split(" ").map{it.toInt()}
+    val n3=br.readLine().split(" ").map{it.toInt()}
+    val x=findDif(n1[0],n2[0],n3[0])
+    val y=findDif(n1[1],n2[1],n3[1])
     bw.write("$x $y")
     br.close()
     bw.close()
@@ -39,4 +39,8 @@ fun findDif(n1:Int,n2:Int,n3:Int):Int{
 
   조건 : 축에 평행한 직사각형
   사고 : 같은수 각각 2개씩있어야하므로 없는거 출력 ->세 값 받아서 조건문써서 같으면 나머지하나 방출
+
+
+  일일히 toInt()거는것보다 .map{it.toInt()}가 약간 더 오래걸리는듯,,
+  그래도 원소 많을때는  .map()이 유용할듯
 */
