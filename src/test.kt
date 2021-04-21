2d495fccfcb135d903471b3a10fc62f104849cc3
yubinkim <kimu2371@naver.com>
2021-04-21 11:35:05 +0900

    #4153 직각 삼각형
    문제
    과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
    
    입력
    입력은 여러개의 테스트케이스로 주어지며 마지막줄에는 0 0 0이 입력된다. 각 테스트케이스는 모두 30,000보다 작은 양의 정수로 주어지며, 각 입력은 변의 길이를 의미한다.
    
    출력
    각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
    
    https://www.acmicpc.net/problem/4153

--- src/test.kt
+++ src/test.kt
@@ -5,42 +5,48 @@ import java.io.OutputStreamWriter
 import kotlin.math.sqrt
 
 
-//#3009
+//#4153
 fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
-    val n1=br.readLine().split(" ").map{it.toInt()}
-    val n2=br.readLine().split(" ").map{it.toInt()}
-    val n3=br.readLine().split(" ").map{it.toInt()}
-    val x=findDif(n1[0],n2[0],n3[0])
-    val y=findDif(n1[1],n2[1],n3[1])
-    bw.write("$x $y")
+    while(true){
+        var input=br.readLine().split(" ")
+        if(input[0]=="0"&&input[1]=="0"&&input[2]=="0")break
+        if(pita(input[0].toInt(),input[1].toInt(),input[2].toInt())) bw.write("right\n")
+        else bw.write("wrong\n")
+        bw.flush()
+    }
     br.close()
     bw.close()
 
 }
 
-fun findDif(n1:Int,n2:Int,n3:Int):Int{
-    when{
-        n1==n2->return n3
-        n2==n3->return n1
-        n1==n3->return n2
-        else ->return -1
+fun pita(n1:Int,n2:Int,n3:Int):Boolean{
+    var nums= mutableListOf(n1,n2,n3)
+    var temp:Int
+
+    for(i in 0 until 3){
+        for(j in i+1 until 3){
+            if(nums[i]>nums[j]){
+                temp=nums[i]
+                nums[i]=nums[j]
+                nums[j]=temp
+            }
+        }
+
     }
+
+    if(nums[2]*nums[2]==nums[1]*nums[1]+nums[0]*nums[0]) return true
+    return false
+
 }
 
 
 
 
+
+
 /*
-  입력 : 세점의 좌표
-  출력 : 네번째점의 좌표
-
-  조건 : 축에 평행한 직사각형
-  사고 : 같은수 각각 2개씩있어야하므로 없는거 출력 ->세 값 받아서 조건문써서 같으면 나머지하나 방출
-
-
-  일일히 toInt()거는것보다 .map{it.toInt()}가 약간 더 오래걸리는듯,,
-  그래도 원소 많을때는  .map()이 유용할듯
+피타고라스 정리사용 -> 다른 사람들은 조건문 사용해서 시간 줄인듯
 */
