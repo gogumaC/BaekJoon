a64178058868f7f2774fc12ef4f07a5da3d3db27
yubinkim <kimu2371@naver.com>
2021-04-22 12:24:41 +0900

    #10872 팩토리얼
    문제
    0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
    
    출력
    첫째 줄에 N!을 출력한다.
    
    https://www.acmicpc.net/problem/10872

--- src/test.kt
+++ src/test.kt
@@ -7,44 +7,28 @@ import kotlin.math.sqrt
 
 
 
-//#1002
+//#10872
 fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
-    for(t in 0 until br.readLine().toInt()){
-        bw.write(getPointNum(br.readLine()))
-        bw.flush()
-    }
+    val result=getFac(br.readLine().toInt()).toString()
+    bw.write(result)
+
     br.close()
     bw.close()
 
 }
 
-fun getPointNum(input:String):String {
-    val inputList = input.split(" ")
-    val x1 = inputList[0].toInt()
-    val y1 = inputList[1].toInt()
-    val r1 = inputList[2].toDouble()
-    val x2 = inputList[3].toInt()
-    val y2 = inputList[4].toInt()
-    val r2 = inputList[5].toDouble()
+fun getFac(input:Int):Int{
+    if (input==0)return 1
+    var result:Int
+    if(input==1) return 1
+        result=input*getFac(input-1)
 
-    val pointsDistance = sqrt(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)).toDouble())
-    when {
-        pointsDistance == 0.0&&r1==r2 -> return "-1\n"
-        pointsDistance > (r1 + r2)||pointsDistance<(r2-r1).absoluteValue-> return "0\n"
-        pointsDistance == (r1 + r2)||pointsDistance==(r2-r1).absoluteValue -> return "1\n"
-        pointsDistance < (r1 + r2)&&pointsDistance>(r2-r1).absoluteValue -> return "2\n"
 
-        else ->return "err\n"
-    }
-
+    return result
 }
-
-
-
-
 
 
 
