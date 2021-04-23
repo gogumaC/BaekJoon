e0fee59a99e564dd21e37ded568ca9655c3e3351
yubinkim <kimu2371@naver.com>
2021-04-23 18:25:05 +0900

    #2447 별찍기
    문제
    재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다.
    
    크기 3의 패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩 있는 패턴이다.
    
    ***
    * *
    ***
    N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 예를 들어 크기 27의 패턴은 예제 출력 1과 같다.
    
    입력
    첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에 대해 N=3k이며, 이때 1 ≤ k < 8이다.
    
    출력
    첫째 줄부터 N번째 줄까지 별을 출력한다.
    
    https://www.acmicpc.net/problem/2447

--- src/test.kt
+++ src/test.kt
@@ -1,31 +1,81 @@
 import java.io.*
-import kotlin.math.absoluteValue
-import kotlin.math.sqrt
 
-
-
-//#10870
+//#2447
 fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
 
-    bw.write("${pibo(br.readLine().toInt())}\n")
+    //bw.write(square(br.readLine().toInt())) -> 배열이 훨빨랐다,,
+    var array=getResult(br.readLine().toInt())
+    for(i in array){
+        bw.write("$i\n")
+    }
+
     br.close()
     bw.close()
 
 }
 
-fun pibo(count:Int):Int{
-    if(count==1)return 1
-    else if(count==0) return 0
-    val piboNum=pibo(count-1)+pibo(count-2)
-    return piboNum
+fun square(input:Int):String {
+
+    if (input == 3) return "***\n* *\n***"
+
+
+    //4는공백
+
+
+    var result = ""
+
+    val splitMini = square(input / 3).split("\n")
+    for (j in 0 until splitMini.size) {
+        if (splitMini[j] == "") continue
+        result += splitMini[j] + splitMini[j] + splitMini[j]
+        result += "\n"
+
+    }
+
+    val midLine = result.split("\n")
+    var midresult = ""
+    for (i in midLine.indices) {
+        if(midLine[i]=="")continue
+        midresult += midLine[i].replaceRange(input / 3.. 2 * input / 3 - 1, " ".repeat(input/3))
+
+        midresult += "\n"
+    }
+
+
+
+
+
+    return result + midresult + result
+
+}
+
+
+fun getResult(input:Int):Array<String>{ //배열이용 ->훨씬 빠름
+
+    if(input==3) return arrayOf("***","* *","***")
+
+    val mini=getResult(input/3)
+    val array=Array(input){""}
+
+
+    for(i in 0 until input/3){
+        array[i]=mini[i].repeat(3)
+    }
+    for(i in input/3 until 2*input/3){
+        array[i]=mini[i%(input/3)]+" ".repeat(input/3)+mini[i%(input/3)]
+
+    }
+    for(i in 2*input/3 until input){
+        array[i]=array[i-2*input/3]
+    }
+
+    return array
 }
 
 
 
-
 /*
-피보나치 수
-0 1 |1 2
+가운데 사각형(N/3*N/3)을 N/3 패턴으로 둘러쌈
 */
