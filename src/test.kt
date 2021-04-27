8001886588f8674686a909821beae68d4f4520b5
yubinkim <kimu2371@naver.com>
2021-04-27 11:58:21 +0900

    #1018 체스판 다시 칠하기
    문제
    지민이는 자신의 저택에서 MN개의 단위 정사각형으로 나누어져 있는 M*N 크기의 보드를 찾았다. 어떤 정사각형은 검은색으로 칠해져 있고, 나머지는 흰색으로 칠해져 있다. 지민이는 이 보드를 잘라서 8*8 크기의 체스판으로 만들려고 한다.
    
    체스판은 검은색과 흰색이 번갈아서 칠해져 있어야 한다. 구체적으로, 각 칸이 검은색과 흰색 중 하나로 색칠되어 있고, 변을 공유하는 두 개의 사각형은 다른 색으로 칠해져 있어야 한다. 따라서 이 정의를 따르면 체스판을 색칠하는 경우는 두 가지뿐이다. 하나는 맨 왼쪽 위 칸이 흰색인 경우, 하나는 검은색인 경우이다.
    
    보드가 체스판처럼 칠해져 있다는 보장이 없어서, 지민이는 8*8 크기의 체스판으로 잘라낸 후에 몇 개의 정사각형을 다시 칠해야겠다고 생각했다. 당연히 8*8 크기는 아무데서나 골라도 된다. 지민이가 다시 칠해야 하는 정사각형의 최소 개수를 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 N과 M이 주어진다. N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에는 보드의 각 행의 상태가 주어진다. B는 검은색이며, W는 흰색이다.
    
    출력
    첫째 줄에 지민이가 다시 칠해야 하는 정사각형 개수의 최솟값을 출력한다.
    
    https://www.acmicpc.net/problem/1018

--- src/test.kt
+++ src/test.kt
@@ -1,39 +1,49 @@
 import java.io.*
 
-//#2231
+//#1018
 
 fun main() {
     val br=BufferedReader(InputStreamReader(System.`in`))
     val bw=BufferedWriter(OutputStreamWriter(System.`out`))
+    val (n,m)=br.readLine().split(" ")
+    val bord=Array(n.toInt()){""}
 
-    val case=br.readLine().toInt()
-    val bigs=Array(case,{Big(0,0)})
-    for(i in 0 until case){
-        val (w,h)=br.readLine().split(" ")
-        bigs[i].w=w.toInt()
-        bigs[i].h=h.toInt()
+    for(i in bord.indices){
+        bord[i]=br.readLine()
+
     }
 
-    bw.write("${getRank(bigs)}")
+
+
+    bw.write("${getState(n.toInt(),m.toInt(),bord)}")
+
 
     br.close()
     bw.close()
 }
 
-fun getRank(bigs:Array<Big>):String{
-    var result=""
-    for(i in bigs.indices){
-        var tempCount=0
-        for(j in bigs.indices){
-            if(bigs[i].w<bigs[j].w&&bigs[i].h<bigs[j].h) tempCount++
-        }
-        if(i==bigs.size-1){result+="${tempCount+1}"}
-        else result+="${tempCount+1} "
-    }
-    return result
-}
 
-data class Big(var w:Int,var h:Int)
+fun getState(n:Int,m:Int,bord:Array<String>):Int{
+    val whiteBord=arrayOf("WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW")
+    val blackBord=arrayOf("BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB")
+    var min=64
+    for(ver in 0 .. n-8){
+        for(hor in 0 .. m-8){
+            var wcount=0
+            var bcount=0
+            for(i in ver until ver+8){
+                for(j in hor until hor+8){
+                    if(whiteBord[i-ver][j-hor]!=bord[i][j]) wcount++
+                    if(blackBord[i-ver][j-hor]!=bord[i][j]) bcount++
+                }
+            }
+            if(wcount<min)min=wcount
+            if(bcount<min)min=bcount
+
+        }
+    }
+    return min
+}
 
 
 
